3dc904ec3d3979eba2fec43377d7330de6829c29
ziaochina@gmail.com <ziaochina@gmail.com>
2018-11-14 16:34:44 +0800

    zlj

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import pkgJson from './package.json'
 import { actionMixin, registerComponent } from 'maka'
 import FixedDataTable from 'maka-fixed-data-table'
+import FDT from './wrapper'
 
 import './style.less'
 
 const name = pkgJson.name
 
 registerComponent('FixedDataTable', FixedDataTable)
+registerComponent('FDT', FDT)
 
 const state = {
     data: {
@@ -29,39 +31,81 @@ class action {
 }
 
 const view = {
-    component: 'FixedDataTable.Table',
-    rowHeight: 50,
-    headerHeight: 50,
-    rowsCount: '{{data.list.length}}',
-    width: 1000,
-    height: 500,
+    component: 'div',
     children: [{
-        component: 'FixedDataTable.Column',
-        columnKey: 'name',
-        header: {
-            component: 'FixedDataTable.Cell',
-            children: 'Name'
-        },
-        cell: {
-            _function: '(row)',
-            component: 'FixedDataTable.Cell',
-            children: '{{ data.list[row.rowIndex].name}}'
-        },
-        width: 100
+        component: 'FixedDataTable.Table',
+        rowHeight: 50,
+        headerHeight: 50,
+        rowsCount: '{{data.list.length}}',
+        width: 1000,
+        height: 300,
+        children: [{
+            component: 'FixedDataTable.Column',
+            columnKey: 'name',
+            header: {
+                component: 'FixedDataTable.Cell',
+                children: 'Name'
+            },
+            cell: {
+                _function: '(row)',
+                component: 'FixedDataTable.Cell',
+                children: '{{ data.list[row.rowIndex].name}}'
+            },
+            width: 100
+        }, {
+            component: 'FixedDataTable.Column',
+            columnKey: 'descrption',
+            header: {
+                component: 'FixedDataTable.Cell',
+                children: 'Descrption'
+            },
+            cell: {
+                _function: '(row)',
+                component: 'FixedDataTable.Cell',
+                children: '{{ data.list[row.rowIndex].description}}'
+            },
+            width: 100
+        }]
     }, {
-        component: 'FixedDataTable.Column',
-        columnKey: 'descrption',
-        header: {
-            component: 'FixedDataTable.Cell',
-            children: 'Descrption'
-        },
-        cell: {
-            _function: '(row)',
-            component: 'FixedDataTable.Cell',
-            children: '{{ data.list[row.rowIndex].description}}'
-        },
-        width: 100
+        component: 'div',
+        style: { width: 1000, height: 300 },
+        children: {
+            component: 'FDT.Table',
+            rowHeight: 50,
+            headerHeight: 50,
+            rowsCount: '{{data.list.length}}',
+            width: 1000,
+            height: 500,
+            columns: [{
+                component: 'FDT.Column',
+                columnKey: 'name',
+                header: {
+                    component: 'FDT.Cell',
+                    children: 'Name'
+                },
+                cell: {
+                    _function: '(row)',
+                    component: 'FDT.Cell',
+                    children: '{{data.list[row.rowIndex].name}}'
+                },
+                width: 100
+            }, {
+                component: 'FDT.Column',
+                columnKey: 'descrption',
+                header: {
+                    component: 'FDT.Cell',
+                    children: 'Descrption'
+                },
+                cell: {
+                    _function: '(row)',
+                    component: 'FDT.Cell',
+                    children: '{{data.list[row.rowIndex].description}}'
+                },
+                width: 100
+            }]
+        }
     }]
+
 }
 
 export {
